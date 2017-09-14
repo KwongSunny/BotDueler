f3c9445e6b7838e992a3c4838defebef22034035
BT_1N3_16 <BT_1N3_16@BT_1N3_16.bktech.local>
2017-09-14 11:43:23 -0400

    Corrected

--- Bot Duel Sunny Kwong/src/Main/CharacterB.java
+++ Bot Duel Sunny Kwong/src/Main/CharacterB.java
@@ -2,27 +2,26 @@ package Main;
 
 public class CharacterB implements Dueler {
 	
-	public static final String[] GLOBALTAUNTS = {"I dare you to shoot me!", "You have no chance!", "You will lose!", "You better be afraid!"};
-	public int arraySize = GLOBALTAUNT.length;
+	private int hp;
 	
-	public void taunt() {
-		if(int arrayIndex = )
-		private int arrayIndex = GLOBALTAUNTS[Math.ceil(Math.random() * arraySize)];
+	public CharacterB() {
 		
-		private String theTaunt = GLOBALTAUNTS[arrayIndex];
-		System.out.println(theTaunt);
 	}
 	
 	public String getName() {
-		return "Hello";
+		return "Name";
+	}
+	
+	public void taunt() {
+		System.out.println("");
 	}
 	
 	public void setStartingHP(int hp) {
-		
+		this.hp = hp;
 	}
 	
 	public int getHP() {
-		
+		return hp;
 	}
 	
 }
