a29d80dfe2969a7b8260ed6af1a9c9c8d5fa93d2
BT_1N3_16 <BT_1N3_16@BT_1N3_16.bktech.local>
2017-09-15 11:48:10 -0400

    `

--- Bot Duel Sunny Kwong/src/Main/CharacterB.java
+++ Bot Duel Sunny Kwong/src/Main/CharacterB.java
@@ -1,32 +1,46 @@
-package Main;
+package main;
 
 public class CharacterB implements Dueler {
-	
-	private int hp == -9999999;
+
+	private int hp = -1;
 	
 	public CharacterB() {
-		
-	}
 	
-	public String getName() {
-		return "Name";
 	}
 	
 	public void taunt() {
-		System.out.println("");
+		
+		System.out.println("You will never shoot me!");
+	}
+	
+	public String getName() {
+		
+		return "Justin";
 	}
 	
 	public void setStartingHP(int hp) {
-		if(this.hp == -9999999) {
-		this.hp = hp;
+		
+		if(this.hp == -1) {
+			this.hp = hp;
 		}
+		
 	}
 	
 	public int getHP() {
 		return hp;
 	}
-	public boolean determineIfOpponentIsFair(Dueler d, int hp) {
-		
+	
+	public boolean determineIfOpponentIsFair(Dueler d, int hp)
+	{
+		return d.getHP() == hp;
+	}
+	
+	public int getAction(Object caller) {
+		if(caller instanceof CharacterA) {
+			return 3;
+		}
+		else
+			return (Math.round(Math.random()+1));
 	}
 	
 }
